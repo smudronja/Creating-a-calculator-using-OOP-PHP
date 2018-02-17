8b6d13fdc38aec2cc4cd757b3d9ea2f41833bc06
Stjepan Mudronja <smudronja.sm@gmail.com>
2018-02-17 08:57:10 +0100

    Created calculator using OOP PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<form action="calc.php" method="POST">
+	<input type="text" name="num1">
+	<input type="text" name="num2">
+	<select name="cal">
+		<option value="add">Add</option>
+		<option value="sub">Subtract</option>
+		<option value="mul">Multiplay</option>	
+	</select>
+	<button type="submit">Calculate</button>
+</form>	
+
+</body>
+</html>
